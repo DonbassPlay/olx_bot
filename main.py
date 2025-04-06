9c49b39551ea68376e0ad27820037c066eeec60d
DonbassPlay <Dyagilevzheka@gmail.com>
2025-04-07 01:09:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telegram
 import time
 from flask import Flask
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, JobQueue
 from threading import Thread
 
 # Указываем токен от BotFather
@@ -48,21 +48,16 @@ def send_to_telegram(new_ads):
 
 # Обработчик команды /start
 def start(update: Update, context: CallbackContext):
+    print("Command /start received")  # Логирование получения команды /start
     update.message.reply_text("Привет! Я буду присылать тебе новые объявления iPhone с OLX!")
 
-# Главная функция для парсинга в фоновом режиме
-def parse_and_send_ads():
-    while True:
-        new_ads = get_new_iphone_ads()
-        if new_ads:
-            send_to_telegram(new_ads)
-        time.sleep(120)  # Пауза 2 минуты
+# Функция для парсинга в фоновом режиме
+def parse_and_send_ads(context: CallbackContext):
+    new_ads = get_new_iphone_ads()
+    if new_ads:
+        send_to_telegram(new_ads)
 
-# Главная функция, которая будет запускать бота в фоновом режиме
-@app.route('/')
-def home():
-    return 'Бот работает!'
-
+# Главная функция для запуска бота
 def start_bot():
     # Настроим Updater и Dispatcher
     updater = Updater(bot_token, use_context=True)
@@ -70,21 +65,24 @@ def start_bot():
     
     # Обработчик команды /start
     dp.add_handler(CommandHandler("start", start))
-    
+
+    # Настройка JobQueue для регулярного запуска парсинга
+    job_queue = updater.job_queue
+    job_queue.run_repeating(parse_and_send_ads, interval=120, first=0)  # Повторение каждые 2 минуты
+
     # Запуск бота
     updater.start_polling()
 
-# Запуск бота и Flask
+# Главная функция, которая будет запускать Flask-сервер
+@app.route('/')
+def home():
+    return 'Бот работает!'
+
 if __name__ == '__main__':
-    # Запуск парсинга в фоновом потоке
-    parse_thread = Thread(target=parse_and_send_ads)
-    parse_thread.daemon = True
-    parse_thread.start()
-    
-    # Запуск бота в фоновом потоке
-    bot_thread = Thread(target=start_bot)
-    bot_thread.daemon = True
-    bot_thread.start()
-    
-    # Запуск Flask-сервера
-    app.run(host='0.0.0.0', port=80)
+    # Запуск Flask в отдельном потоке
+    flask_thread = Thread(target=lambda: app.run(host='0.0.0.0', port=80))
+    flask_thread.daemon = True
+    flask_thread.start()
+
+    # Запуск бота
+    start_bot()
